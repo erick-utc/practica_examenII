9c5301c07442ce6de88bc4ea83bcc573bbdf71dd
Erick Espinoza <eespinozaa0489@uvirtual.ac.cr>
2023-11-12 10:31:06 -0600

    adding asp project

---
+++ practica_II_examen/Suministros.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Suministros.aspx.cs" Inherits="practica_examen_II.Suministros" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Practica II Examen</title>
+    <link href="css/main.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+<body class="bg-primary">
+    <form id="form1" runat="server">
+        <div class="container">
+            <div class="row">
+                 <div class="col--12 text-white text-center">
+                     <h2>Suministros</h2>
+                 </div>
+                <div class="col--12 p-4">
+                    <asp:GridView runat="server" ID="dgSuministros" PageSize="10" HorizontalAlign="Center"
+                        CssClass="mydatagrid" PagerStyle-CssClass="pager" HeaderStyle-CssClass="header"
+                        RowStyle-CssClass="rows" AllowPaging="True"    />
+                </div>
+                <div class="col-4 text-center">
+                    <label for="piezas" class="form-label text-white">Pieza</label>
+                    <asp:DropDownList runat="server" ID="piezas" CssClass="form-select" />
+                </div>
+                <div class="col-4 text-center text-white">
+                    <label for="proveedores" class="form-label">Profeedores</label>
+                    <asp:DropDownList runat="server" ID="proveedores" CssClass="form-select" />
+                </div>
+                <div class="col-4 text-center text-white">
+                    <label for="precio" class="form-label">Precio</label>
+                    <div class="input-group mb-3">
+                      <span class="input-group-text">₡</span>
+                      <asp:TextBox runat="server" ID="precio" CssClass="form-control" Text="" />
+                      <span class="input-group-text">.00</span>
+                    </div>
+                    
+                </div>
+                <div class="col--5"></div>
+                <div class="col--2 text-center mt-4">
+                    <asp:Button runat="server" ID="btnAgregar" Text="Agregar Suministro" class="btn btn-success" OnClick="btnAgregar_Click" />
+                </div>
+                <div class="col--5"></div>
+            </div>
+
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ practica_II_examen/Suministros.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace practica_examen_II
+{
+    public partial class Suministros : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            fillGrid();
+            fillPiezas();
+            fillProveedores();
+        }
+
+        protected void fillGrid()
+        {
+            string cnxStr = ConfigurationManager.ConnectionStrings["cnxSuministros"].ConnectionString;
+            using (SqlConnection cnx = new SqlConnection(cnxStr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT piezas.Nombre as NombreDePieza, proveedores.Nombre as NomreDeProveedor, suministros.Precio FROM suministros JOIN piezas ON piezas.Codigo = suministros.CodigoPieza\r\nJOIN proveedores ON proveedores.Id = suministros.IdProveedor"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = cnx;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            dgSuministros.DataSource = dt;
+                            dgSuministros.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void fillPiezas()
+        {
+            string cnxStr = ConfigurationManager.ConnectionStrings["cnxSuministros"].ConnectionString;
+            using(SqlConnection cnx = new SqlConnection(cnxStr))
+            {
+                using(SqlCommand cmd = new SqlCommand("SELECT * FROM piezas"))
+                {
+                    using(SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = cnx;
+                        sda.SelectCommand = cmd;
+                        using(DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            piezas.DataSource = dt;
+
+                            piezas.DataTextField = dt.Columns["Nombre"].ToString();
+                            piezas.DataValueField = dt.Columns["Codigo"].ToString();
+                            piezas.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void fillProveedores()
+        {
+            string cnxStr = ConfigurationManager.ConnectionStrings["cnxSuministros"].ConnectionString;
+            using (SqlConnection cnx = new SqlConnection(cnxStr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM proveedores"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = cnx;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            proveedores.DataSource = dt;
+
+                            proveedores.DataTextField = dt.Columns["Nombre"].ToString();
+                            proveedores.DataValueField = dt.Columns["Id"].ToString();
+                            proveedores.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void add()
+        {
+            try
+            {
+                string cnxStr = ConfigurationManager.ConnectionStrings["cnxSuministros"].ConnectionString;
+                SqlConnection cnx = new SqlConnection(cnxStr);
+                if(!precio.Text.Trim().Equals(""))
+                {
+                    float precioValue = float.Parse(precio.Text);
+                    int piezaValue = int.Parse(piezas.SelectedValue.ToString().Trim());
+                    string proveedoresValue = proveedores.SelectedValue.ToString().Trim();
+
+                    string queryString = "INSERT INTO suministros(CodigoPieza, IdProveedor, Precio) VALUES (" + piezaValue + ",'" + proveedoresValue + "'," + precioValue + ")";
+
+                    SqlCommand cmd = new SqlCommand(queryString, cnx);
+
+                    cnx.Open();
+                    cmd.ExecuteNonQuery();
+                    cnx.Close();
+                    Response.Redirect(Request.RawUrl);
+
+                }
+            }catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        protected void btnAgregar_Click(object sender, EventArgs e)
+        {
+            add();
+        }
+    }
+}
